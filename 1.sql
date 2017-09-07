024d5798607e3ff31686987a466176fa6bb39fab
tyyking <451655144@qq.com>
2017-09-07 11:44:14 +0800

    sql
    sql

---
+++ 1.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.34 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('30','2','1','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('31','2','2','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('32','2','6','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('33','2','7','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('56','2','36','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('60','2','40','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('61','2','41','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('63','2','43','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('70','2','51','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('72','2','53','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('79','2','60','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('83','2','64','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('85','2','66','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('86','2','67','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('87','2','68','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('88','2','69','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('89','2','71','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('90','2','72','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('91','2','73','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('92','2','74','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('93','2','75','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('94','2','76','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('95','2','118','1','1','1','1');
+insert into `sys_role_menu` (`id`, `role_id`, `menu_id`, `add_qx`, `del_qx`, `edit_qx`, `cha_qx`) values('135','2','38','1','1','1','1');
